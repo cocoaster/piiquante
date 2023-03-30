95cb5bfa8ebb14d4108d5ae6c75e604cd49cfa4c
coralie montalbano <montalbanocoralie@gmail.com>
2023-03-30 03:25:44 +0100

    suppr des commentaires inutiles

--- backend/models/users.js
+++ backend/models/users.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const arrayUniquePlugin = require("mongoose-unique-array");
 
 // // Valider un mot de passe
-// const password = "UnMotDePasseValide123";
 
 const usersSchema = mongoose.Schema({
   email: {type: String, required: true, unique: true},
